6b969312b014c3ad3930e5fe6c4dd67ee3e618cc
challengerJR <peerlesspixel@gmail.com>
2024-11-06 19:57:49 -0500

    add scripts.js

---
+++ script.js
@@ -0,0 +1,27 @@
+
+//makes random choice between three items
+function getComputerChoice(){let a=Math.random(2);
+    if a=0 {return "rock"} else if a=1 {return "paper"} else if a=2 {return "scissors"}
+console.log(getComputerChoice)
+}
+
+function getHumanChoice(){
+
+}
+
+function humanScore(){
+
+}
+
+function computerScore(){
+
+}
+
+function playRound(HumanChoice, computerChoice){
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
