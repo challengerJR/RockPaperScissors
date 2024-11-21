e36e66f7a02e6ae84dab79fd07ae3c16d87269ab
challengerJR <peerlesspixel@gmail.com>
2024-11-21 13:22:31 -0500

    added 5 round counter moved output from console to DOM

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let tie = 0;
 let humanScore = 0;
 let computerChoice;
 let computerString;
+let humanString;
+let status = document.getElementById("status");
+
+
 //makes random choice between three items
 function getcomputerChoice(max){return Math.floor((Math.random() * max+1));
 }
@@ -16,13 +20,13 @@ document.getElementById("reset").addEventListener("click", reset);
 //human choice, triggers full round
 function choose()
 {
-    if (this.id == "rock") {humanChoice = 1} else if (this.id == "paper") {humanChoice = 2} else if (this.id == "scissors") {humanChoice = 3}
-    console.log("Human chooses " + this.id)
-    computerChoice = getcomputerChoice(3);
- 
-    if (computerChoice == "1") {computerString = "rock"} else if (computerChoice == "1") {computerString = "paper"} else if (computerChoice == "1") {computerString = "scissors"};
-    console.log("computer chooses " + computerString)
+    if (this.id == "rock") {humanChoice = 1; humanString = "rock"} else if (this.id == "paper") {humanChoice = 2; humanString = "paper"} else if (this.id == "scissors") {humanChoice = 3; humanString = "scissors"};
+
+computerChoice = getcomputerChoice(3);
+ console.log(computerChoice);
+    if (computerChoice === 1) {computerString = "rock"} else if (computerChoice === 2) {computerString = "paper"} else if (computerChoice === 3) {computerString = "scissors"};
     playRound();
+  return computerString;
 }
 
 //plays full round
@@ -34,8 +38,7 @@ function playRound(){
 //logs winner, increments score
 function whoWon() 
 
-{console.log ("human choice is " + humanChoice + " computer choice is " + computerChoice)
-if (computerChoice == humanChoice) {console.log("It's a tie!");tie++}
+{if (computerChoice == humanChoice) {console.log("It's a tie!");tie++}
 else if ((computerChoice == 1 && humanChoice == 3) || (computerChoice == 2 && humanChoice == 1) || (computerChoice == 3 && humanChoice == 2)){console.log("Computer wins!");computerScore++;}
 else if ((humanChoice == 1 && computerChoice == 3) || (humanChoice == 2 && computerChoice == 1) || (humanChoice == 3 && computerChoice == 2)) {console.log("Human wins!");humanScore++;}
 }
@@ -43,14 +46,22 @@ else if ((humanChoice == 1 && computerChoice == 3) || (humanChoice == 2 && compu
 
 //keeps score
 function scoreKeeper(){
+  status.innerText = "human choice is " + humanString + ", computer choice is " + computerString;
+  
     document.getElementById("humanScore").innerText=humanScore;
     document.getElementById("computerScore").innerText=computerScore;
     document.getElementById("tie").innerText=tie;
+ 
+if (humanScore === 5) {alert("Human wins! Start over?");reset();}
+  else if (computerScore === 5) {alert("Computer wins! Start over?");reset();}
+;
 }
 
 function reset(){
     humanScore = 0;
     computerScore = 0;
     tie = 0;
-    scoreKeeper()
+    scoreKeeper();
+   document.getElementById("status").innerText="";
+  
 }
